706cfeaea697c6e7b9a9a3f24208fe05a2f7a959
Chris <chriseckert.ce@gmail.com>
2018-02-15 16:46:04 +0100

    contact page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,10 +10,10 @@ const footer = () => {
             <h2>&copy; 2018 Chris Eckert</h2>
           </div>
           <div className="level-right has-text-centered">
-            <a href="https://github.com/iamchrisjp" className="level-item"><i className="fab fa-github"></i></a>
-            <a href="https://dribbble.com/Eckert" className="level-item"><i className="fab fa-dribbble"></i></a>
-            <a href="https://www.linkedin.com/in/chris-eckert-812a76135" className="level-item"><i className="fab fa-linkedin-in"></i></a>
-            <a href="https://www.behance.net/chrisecker3eab" className="level-item"><i className="fab fa-behance"></i></a>
+            <a href="https://github.com/iamchrisjp" target="_blank" className="level-item"><i className="fab fa-github"></i></a>
+            <a href="https://dribbble.com/Eckert" target="_blank" className="level-item"><i className="fab fa-dribbble"></i></a>
+            <a href="https://www.linkedin.com/in/chris-eckert-812a76135" target="_blank" className="level-item"><i className="fab fa-linkedin-in"></i></a>
+            <a href="https://www.behance.net/chrisecker3eab" target="_blank" className="level-item"><i className="fab fa-behance"></i></a>
           </div>
         </div>
       </div>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,59 @@
 import React from 'react';
 
-const contactPage = () => {
+const ContactPage = () => {
   return(
-    <div className="needs-air">
+    <div>
+    <section className="hero is-fullheight">
+      <div className="hero-body" id="contactPar">
+        <div className="container">
+          <div className="box">
+            <article className="media">
+              <div className="media-left">
+                <figure className="image is-64x64">
+                  <img src="https://media-exp2.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAdBAAAAJGUwNGI5YTMwLTJjNWUtNDRmMS1iMDdiLTJiYjE4YWY3MzNlZA.jpg" alt="Chris Eckert" />
+                </figure>
+              </div>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>Chris Eckert</strong> <small>@iamchrisjp</small> <small>31m</small>
+                    <br/>
+                    Hvis du er intereseret i et samarbejde eller yderligere information, kan du altid kontakte mig på min mail.
+                    <br/>
+                    Du kan også finde mig på min sociale medier, jeg ser frem til at høre fra dig.
+                  </p>
+                </div>
+                <nav className="level is-mobile">
+                  <div className="level-left">
+                    <a className="level-item" href="mailto:chris@chriseckert.dk">
+                      <span className="is-small"><i className="fas fa-envelope"></i> chris@chriseckert.dk</span>
+                    </a>
+                  </div>
+                </nav>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="columns container is-size-3 has-text-centered">
+          <a href="https://github.com/iamchrisjp" target="_blank" className="column">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/chris-eckert-812a76135" target="_blank" className="column">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          <a href="https://www.behance.net/chrisecker3eab" target="_blank" className="column">
+          <i className="fab fa-behance"></i>
+          </a>
+          <a href="https://dribbble.com/Eckert" target="_blank" className="column">
+            <i className="fab fa-dribbble"></i>
+          </a>
+        </div>
+      </div>
+      </section>
     </div>
   )
 }
 
-export default contactPage;
+export default ContactPage;
